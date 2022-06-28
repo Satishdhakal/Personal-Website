99aa8205a0caf4cf954f59b1bdfbbd9e3dbeb83b
Satish Dhakal <dhakalsatish2004@gmail.com>
2022-06-28 20:32:15 +0545

    less

--- script.js
+++ script.js
@@ -74,4 +74,6 @@ $(document).ready(function(){
     $(".wd").click(function(){
         location.href = "web design button/index.html";
       });
+
+      $('.web').on('dragstart', function(event) { event.preventDefault(); });
 });
