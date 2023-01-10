e6525c9ce7d8f95e54d18770737787c45add4309
Satish <dhakalsatish2004@gmail.com>
2023-01-10 12:35:16 +0545

    content creation

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Designer", "Developer", "Learner",  "Philosophical Thinker"],
+        strings: ["Web Designer", "Developer", "Learner",  "Content Creator"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Designer", "Developer", "Learner",  "Philosophical Thinker"],
+        strings: ["Web Designer", "Developer", "Learner",  "Content Creator"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
